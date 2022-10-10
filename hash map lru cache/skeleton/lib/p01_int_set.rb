55f942df0db1dec51c2b1cf7b1f26bf413aae9bf
Julian Cardone <juliancardone1@gmail.com>
2022-10-10 14:27:09 -0400

    we're cookin

--- hash map lru cache/skeleton/lib/p01_int_set.rb
+++ hash map lru cache/skeleton/lib/p01_int_set.rb
@@ -1,4 +1,9 @@
+require 'byebug'
+
 class MaxIntSet
+
+  attr_accessor :store
+
   def initialize(max)
     @store = Array.new(max){false}
   end
@@ -6,23 +11,24 @@ class MaxIntSet
   def insert(num)
     raise "Out of bounds" if !self.is_valid?(num)
     if !@store.include?(num)
+      # debugger
       @store[num] = true
       return true
     end
   end
 
   def remove(num)
-
+    @store[num] = false
   end
 
   def include?(num)
-    @store[num-1]
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
-    num <= 50 && num > 0
+    num < 50 && num > 0
   end
 
   def validate!(num)
@@ -37,18 +43,24 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    return false if !self[num].include?(num)
+    true
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    idx = num % @store.length
+    @store[idx]
   end
 
   def num_buckets
